bce4699c703736b3a7435a7f9112a20df9234452
Dnld <steinert.donald@gmail.com>
2016-01-28 11:53:03 -0500

    completes quiz app

--- ch01/Quiz/Quiz/AppDelegate.swift
+++ ch01/Quiz/Quiz/AppDelegate.swift
@@ -16,6 +16,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        let quizVC = QuizViewController()
+        self.window?.rootViewController = quizVC
         return true
     }
 
--- ch01/Quiz/Quiz/QuizViewController.swift
+++ ch01/Quiz/Quiz/QuizViewController.swift
@@ -12,13 +12,35 @@ class QuizViewController: UIViewController {
 
     @IBOutlet var questionLabel: UILabel!
     @IBOutlet var answerLabel: UILabel!
+    var currentQuestionIndex: Int
+    var questions: [String]
+    var answers: [String]
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        currentQuestionIndex = 0
+        questions = ["From what is cognac made?", "What is 7+7?", "What is the capital of Vermont?"]
+        answers = ["Grapes", "14", "Montpelier"]
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil);
+    }
+
+    // this is required for all UIViewConroller subclasses
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     @IBAction func showQuestion(sender: UIButton) {
-        
+        currentQuestionIndex++
+        if currentQuestionIndex >= questions.count {
+            currentQuestionIndex = 0
+        }
+        let question = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
     }
     
     @IBAction func showAnswer(sender: UIButton) {
-        
+        let answer = answers[currentQuestionIndex]
+        answerLabel.text = answer
     }
     
     override func viewDidLoad() {
