831b658b10d292f307a2900c86f1667078175151
Dnld <steinert.donald@gmail.com>
2016-02-01 09:19:10 -0500

    adds red rect to view

--- ch04/Hypnosister/Hypnosister/AppDelegate.swift
+++ ch04/Hypnosister/Hypnosister/AppDelegate.swift
@@ -16,6 +16,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow.init(frame: UIScreen.mainScreen().bounds)
+        let firstFrame = CGRectMake(160, 240, 100, 150)
+        
+        let firstView = BNRHypnosisView(frame: firstFrame)
+        firstView.backgroundColor = UIColor.redColor()
+        self.window?.addSubview(firstView)
+        
+        self.window?.backgroundColor = UIColor.whiteColor()
+        self.window?.rootViewController = ViewController()
+        self.window?.makeKeyAndVisible()
         return true
     }
 
---
+++ ch04/Hypnosister/Hypnosister/BNRHypnosisView.swift
@@ -0,0 +1,21 @@
+//
+//  BNRHypnosisView.swift
+//  Hypnosister
+//
+//  Created by Donald Steinert on 2/1/16.
+//  Copyright © 2016 Donald Steinert. All rights reserved.
+//
+
+import UIKit
+
+class BNRHypnosisView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
