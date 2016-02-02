6e908cdcc79fd202509cbe1fb4ce823a941653dd
Dnld <steinert.donald@gmail.com>
2016-02-02 09:30:46 -0500

    adds text field

--- ch06/HypnoNerd/HypnoNerd/BNRHypnosisViewController.swift
+++ ch06/HypnoNerd/HypnoNerd/BNRHypnosisViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BNRHypnosisViewController: UIViewController {
+class BNRHypnosisViewController: UIViewController, UITextFieldDelegate {
 
     override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
@@ -28,11 +28,25 @@ class BNRHypnosisViewController: UIViewController {
         let backgroundView = BNRHypnosisView.init(frame: frame)
         view = backgroundView
 //        print("BNRHypnosisViewController loaded")
+
+        let textField = UITextField(frame: CGRectMake(40, 70, 300, 30))
+        textField.borderStyle = UITextBorderStyle.RoundedRect
+        textField.placeholder = "Hypnotize me"
+        textField.returnKeyType = UIReturnKeyType.Done
+        textField.delegate = self
+        view.addSubview(textField)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        if let text = textField.text {
+            print(text)
+        }
+        return true
     }
 
 }
