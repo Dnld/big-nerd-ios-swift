2171461d50ede06e3d5918f1191f2a996eb3f105
Dnld <steinert.donald@gmail.com>
2016-02-03 14:12:04 -0500

    wires up skeleton of navigation view controller

---
+++ ch08/Homepwner/Homepwner/BNRDetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  BNRDetailViewController.swift
+//  Homepwner
+//
+//  Created by Donald Steinert on 2/3/16.
+//  Copyright © 2016 Donald Steinert. All rights reserved.
+//
+
+import UIKit
+
+class BNRDetailViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var serialField: UITextField!
+    @IBOutlet weak var valueField: UITextField!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+}
