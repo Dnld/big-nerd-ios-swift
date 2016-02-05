16e7f989ffe6392c162b4dbeb09649216dff7b46
Dnld <steinert.donald@gmail.com>
2016-02-05 12:27:51 -0500

    init TouchTracker

---
+++ ch12/TouchTracker/TouchTracker/BNRDrawViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  TouchTracker
+//
+//  Created by Donald Steinert on 2/5/16.
+//  Copyright © 2016 Donald Steinert. All rights reserved.
+//
+
+import UIKit
+
+class BNRDrawViewController: UIViewController {
+
+    override func loadView() {
+        view = BNRDrawView(frame: CGRectZero)
+    }
+
+}
+
---
+++ ch12/TouchTracker/TouchTracker/BNRLine.swift
@@ -0,0 +1,13 @@
+//
+//  BNRLine.swift
+//  TouchTracker
+//
+//  Created by Donald Steinert on 2/5/16.
+//  Copyright © 2016 Donald Steinert. All rights reserved.
+//
+
+import UIKit
+
+class BNRLine: NSObject {
+
+}
